2e3ce8d74b0c04dd0684bb8711de49af12d30e01
Alejandro Rodríguez Cañete <arodcan0507@g.educaand.es>
2023-10-16 17:34:04 +0200

    Initial Project

---
+++ MyApplication3/MyLibrary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyApplication3
+{
+    public class MyLibrary
+    {
+        public MyLibrary()
+        {
+
+        }
+    }
+}
---
+++ MyApplication3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MyApplication3;
+namespace MyApplication3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            MyLibrary libreria = new MyLibrary();
+            Console.WriteLine("Hello {0}", libreria);
+        }
+    }
+}
